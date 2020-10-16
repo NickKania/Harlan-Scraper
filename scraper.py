32679fbc02830818e22e33b6ecbd4847eb4fa491
Nick Kania <nicholas.kania@maine.edu>
2020-10-15 23:59:13 -0400

    Added .ics support for user, made script run for a single user specified via argument

--- scraper.py
+++ scraper.py
@@ -1,13 +1,15 @@
 import requests
-from bs4 import BeautifulSoup
 import re
 import pandas as pd
+from ics import Calendar, Event
+import argparse
+from dateutil.parser import parse
 
-class Item():
-    def __init__(self,section, date, role):
-        self.date = date
-        self.section = section
-        self.role = role
+parser = argparse.ArgumentParser()
+parser.add_argument('--cal', dest='create_cal', action='store_true')
+parser.add_argument('name', help='Last name of student')
+parser.set_defaults(create_cal=False)
+args = parser.parse_args()
 
 def get_content():
     try:
@@ -42,16 +44,38 @@ def get_content():
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
 
-names = ['Severs', 'Kania']
+def add_event(calendar, topic, date, role):
+    e = Event()
+    if isinstance(topic, float):
+        topic = 'Chat tracker'
+        role = ''
+    elif not 'p' in topic:
+        topic = 'Chap ' + topic
+    e.name = '%s %s' % (topic, role)
+    e.begin = parse(date).replace(hour=15)
+    calendar.events.add(e)
+
+
+calendar = Calendar()
+name = args.name
 table_list = pd.read_html(get_content())
 for i in range(5, len(table_list)):
     table = table_list[i]
     # print (table)
     for index, row in table.iterrows():
-        for name in names:
-            chat_name = '-' + name
-            if name in row.tolist():
-                print ('%s is presenting: %s on %s' % (name, row[0], row[len(row) - 1]))
-            elif chat_name in row.tolist():
-                # print (row)
-                print ('%s is discussant for: %s on %s' % (name, table.loc[index - 1][0], table.loc[index - 1][len(row) - 1]))
+        chat_name = '-' + name
+        if name in row.tolist():
+            topic = row[0]
+            date = row[len(row) - 1]
+            add_event(calendar, topic, date, 'reporter')
+            print ('%s is presenting: %s on %s' % (name, topic, date))
+        elif chat_name in row.tolist():
+            # print (row)
+            topic = table.loc[index - 1][0]
+            date = table.loc[index - 1][len(row) - 1]
+            add_event(calendar, topic, date, 'discussant')
+            print ('%s is discussant for: %s on %s' % (name, topic, date))
+
+if args.create_cal:
+    with open('%s.ics' % name, 'w') as cal_file:
+        cal_file.write(str(calendar))
